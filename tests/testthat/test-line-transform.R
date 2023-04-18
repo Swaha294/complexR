d313b0d93a0a7288cb82f0667df7ba9f700c3056
Swaha294 <ittswahabhattacharya@gmail.com>
2023-04-17 21:00:03 -0400

    test for line_transform()

---
+++ tests/testthat/test-line-transform.R
@@ -0,0 +1,31 @@
+library(complexR)
+library(testthat)
+
+# testing for errors
+
+test_that("testing for invalid inputs", {
+  expect_error(
+    line_transform(m = "a"),
+    regexp = "should be numbers"
+  )
+  expect_error(
+    line_transform(c = "b"),
+    regexp = "should be numbers"
+  )
+  expect_error(
+    line_transform(x_intercept = "c"),
+    regexp = "should be numbers"
+  )
+  expect_error(
+    line_transform(annotations = c("a", 2, 3)),
+    regexp = "should be a list of complex numbers"
+  )
+})
+
+# testing that annotations lie on the given domain
+test_that("testing that annotations lie on the given domain", {
+  expect_error(
+    line_transform(m = 0, annotations = complex(real = 2, imaginary = 2)),
+    regexp = "on the given line"
+  )
+})
